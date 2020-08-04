6a20399d0db9ddce346b0c158df84ed4332f31ad
paragomia <paragomia@github.com>
2020-08-04 22:54:46 +0200

    Update loginui.py

--- src/loginui.py
+++ src/loginui.py
@@ -1,93 +1,90 @@
-# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 
-# Form implementation generated from reading ui file 'untitled.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.0
-#
-# WARNING! All changes made in this file will be lost!
-
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-import sys
-import tkinter
-from PyQt5.QtWidgets import *
-from tkinter import ttk,filedialog
-import subprocess,sys
-
-from threading import Thread
-
-
-
+#ui file made by pyqt5 designer
 class Ui_MainWindow(object):
-
     def setupUi(self, MainWindow):
-        self.MainWindow = MainWindow
-        self.MainWindow.setObjectName("MainWindow")
-        self.MainWindow.resize(640, 480)
-        self.MainWindow.setStyleSheet("background-color: white;")
-        self.centralwidget = QtWidgets.QWidget(self.MainWindow)
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(301, 433)
+        MainWindow.setWindowTitle("MCL PyGUI  v.0.1")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(0, 0, 641, 471))
-        self.groupBox.setStyleSheet("color: rgb(255, 255, 255);")
-        self.groupBox.setTitle("")
-        self.groupBox.setObjectName("groupBox")
-        
-        self.label = QtWidgets.QLabel(self.groupBox)
-        self.label.setGeometry(QtCore.QRect(110, 0, 421, 301))
-        self.label.setObjectName("label")
-        logo = QtGui.QPixmap("./test.png")
-        self.label.setPixmap(logo)
-        self.label.resize(421, 301)
-        self.label.setScaledContents(True)
-		
-		
-        self.comboBox = QtWidgets.QComboBox(self.groupBox)
-        self.comboBox.setGeometry(QtCore.QRect(170, 315, 121, 21))
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(70, 130, 171, 21))
         self.comboBox.setObjectName("comboBox")
-        self.comboBox.setStyleSheet("background-color: gray;")
-        
-        self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(330, 310, 121, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.setStyleSheet("background-color: gray;")
-        
-        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_2.setGeometry(QtCore.QRect(170, 390, 131, 41))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.setStyleSheet("background-color: gray;")
-        
-        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_3.setGeometry(QtCore.QRect(330, 390, 121, 41))
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(0, 120, 701, 351))
+        self.label_3.setStyleSheet("background-color: rgb(240, 240, 240);")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(70, 310, 171, 41))
+        self.pushButton_3.setStyleSheet("font: 10pt \"Comic Sans MS\";\n"
+"border:0px;\ncolor:#ebdeb1;"
+"background-color: #6c7585;")
         self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_3.setStyleSheet("background-color: gray;")
-        
-        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_4.setGeometry(QtCore.QRect(10, 310, 121, 31))
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(70, 360, 171, 41))
+        self.pushButton_4.setStyleSheet("font: 10pt \"Comic Sans MS\";\n"
+"border:0px;\n"
+"background-color: #6c7585;color:#ebdeb1;")
         self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_4.setStyleSheet("background-color: gray;")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(35, 0, 291, 111))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        pixmap = QtGui.QPixmap("icon.png")
+        self.label_2.setPixmap(pixmap)
         
-        self.MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(self.MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        self.MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi()
-        QtCore.QMetaObject.connectSlotsByName(self.MainWindow)
-    def closeEvent(self,Event):
-        subprocess.Popen("komodo-cli.exe -ac_name=MCL stop")
         
+        
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(-170, -10, 701, 131))
+        self.label.setStyleSheet("background-color: rgb(49, 103, 120);\n"
+"font: 40pt \"Microsoft Tai Le\";")
+        
+        
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(70, 160, 171, 41))
+        self.pushButton.setStyleSheet("font: 10pt \"Comic Sans MS\";\n"
+"border:0px;\n"
+"background-color:#6c7585;color:#ebdeb1;")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(70, 260, 171, 41))
+        self.pushButton_2.setStyleSheet("font: 10pt \"Comic Sans MS\";\n"
+"border:0px;\n"
+"background-color:  #6c7585; color:#ebdeb1;")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_3.raise_()
+        self.pushButton_3.raise_()
+        self.pushButton_4.raise_()
+        self.label.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+        self.comboBox.raise_()
+        self.label_2.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
 
-    def retranslateUi(self):
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        self.MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Giriş yap"))
-        self.pushButton_2.setText(_translate("MainWindow", "Cüzdan Yükle"))
-        self.pushButton_3.setText(_translate("MainWindow", "Yedekten Yükle"))
-        self.pushButton_4.setText(_translate("MainWindow", "Yeni Profil"))
-
-
-
-
+        MainWindow.setWindowTitle(_translate("MainWindow", "MCL PyGUI  v.0.1"))
+        self.pushButton_3.setText(_translate("MainWindow", "Cüzdan Yükle"))
+        self.pushButton_4.setText(_translate("MainWindow", "Yedekten Yükle"))
+        self.label.setText(_translate("MainWindow", "Giriş"))
+        self.pushButton.setText(_translate("MainWindow", "Giriş"))
+        self.pushButton_2.setText(_translate("MainWindow", "Yeni Profil Oluştur"))
+        
+"""
+import sys
+app = QtWidgets.QApplication(sys.argv)
+mainwindow = QtWidgets.QMainWindow()
+test = Ui_MainWindow()
+test.setupUi(mainwindow)
+mainwindow.show()
+sys.exit(app.exec_())
+"""
